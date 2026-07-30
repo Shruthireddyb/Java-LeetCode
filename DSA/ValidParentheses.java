6ff3a6e7dae79d40e211375e74f4d434b026e44f
Shruthireddyb <shruthireddyb1@gmail.com>
2026-07-30 06:15:23 +0530

    Day 6: Valid Parentheses

---
+++ DSA/ValidParentheses.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+public class ValidParentheses {
+      public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            // Push opening brackets
+            if(c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } 
+            // Check closing brackets
+            else {
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty(); // true if all matched
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}")); // true
+        System.out.println(isValid("(]"));     // false
+    }
+}
