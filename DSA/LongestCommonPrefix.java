96e7f131b516895796e607203552a7962b081951
Shruthireddyb <shruthireddyb1@gmail.com>
2026-07-31 15:17:13 +0530

    Day7: Longest Common Prefix

---
+++ DSA/LongestCommonPrefix.java
@@ -0,0 +1,13 @@
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] s) {
+        if(s.length==0) return "";
+        String prefix=s[0];
+        for(int i=1;i<s.length;i++){
+            while(s[i].indexOf(prefix)!=0){
+                prefix=prefix.substring(0,prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+}
+
